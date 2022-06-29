883f0f37be4bb0fec1925a40da32c8cb87d15c60
Marco Corbetta <machopper97@gmail.com>
2022-06-29 15:37:01 +0200

    aggiunta ricerca

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { Box, Center, ChakraProvider } from '@chakra-ui/react';
+import { filterMovies } from './api/getMovies';
 import './App.css';
 import Griglia from './components/Griglia.js';
 import Navbar from './components/Navbar.js';
+import { useState } from 'react';
+
 
 function App() {
 
-
+  const [movies, setMovies] = useState([]); //variabile di stato dichiarata con funzione di hook//
+  //useState ritorna un array//
+  const searchMovies = (keywords) => {
+    setMovies(filterMovies(keywords));
+  }
 
   return (
     <ChakraProvider>
       <Center>
         <Box bg="#1A1C28" h="100%" w="100%" >
-          <Navbar />
-          <Griglia />
+          <Navbar searchCallback={searchMovies} />
+          <Griglia movies={movies} />
         </Box>
       </Center>
     </ChakraProvider>
